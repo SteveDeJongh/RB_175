8e34e2ca151c86941472ed1b3fc53f18b72496b2
Steve De Jongh <de.jongh.steve@gmail.com>
2023-06-17 12:11:04 -0700

    testing start

--- file-based-cms/cms.rb
+++ file-based-cms/cms.rb
@@ -10,3 +10,10 @@ get "/" do
   @files = Dir.glob(root + "/data/*").map { |path| File.basename(path) }
   erb :index
 end
+
+get "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+  
+  headers["Content-Type"] = "text/plain" # Setting content type header response.
+  File.read(file_path)  
+end
