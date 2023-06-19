ddb1114ed111339408cce7c3c9d3f4044c03b267
Steve De Jongh <de.jongh.steve@gmail.com>
2023-06-19 14:11:23 -0700

    Rubocop clean up

--- file-based-cms/cms.rb
+++ file-based-cms/cms.rb
@@ -1,12 +1,12 @@
 # cms.rb
 
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
-require "securerandom"
-require "redcarpet"
-require "yaml"
-require "bcrypt"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
+require 'securerandom'
+require 'redcarpet'
+require 'yaml'
+require 'bcrypt'
 
 configure do
   enable :sessions # Enabling session support for Sinatra app.
@@ -16,10 +16,10 @@ end
 ##### Helpers #####
 
 def data_path
-  if ENV["RACK_ENV"] == "test"
-    File.expand_path("../test/data", __FILE__)
+  if ENV['RACK_ENV'] == 'test'
+    File.expand_path('../test/data', __FILE__)
   else
-    File.expand_path("../data", __FILE__)
+    File.expand_path('../data', __FILE__)
   end
 end
 
@@ -31,31 +31,32 @@ end
 def load_file_content(path)
   content = File.read(path)
   case File.extname(path)
-  when ".txt"
-    headers["Content-Type"] = "text/plain"
+  when '.txt'
+    headers['Content-Type'] = 'text/plain'
     content
-  when ".md"
+  when '.md'
     erb render_markdown(content)
   end
 end
 
 def user_signed_in?
-  session.key?(:username)  
+  session.key?(:username)
 end
 
 def require_signed_in_user
   unless user_signed_in?
-    session[:message] = "You must be signed in to do that."
-    redirect "/"
+    session[:message] = 'You must be signed in to do that.'
+    redirect '/'
   end
 end
 
-def load_user_credentials # Loads appropriate YML file containing user/pass based on environment.
-  credentials_path = if ENV["RACK_ENV"] == "test"
-    File.expand_path("../test/users.yml", __FILE__)
-  else
-    File.expand_path("../users.yml", __FILE__)
-  end
+# Loads appropriate YML file containing user/pass based on environment.
+def load_user_credentials
+  credentials_path = if ENV['RACK_ENV'] == 'test'
+                       File.expand_path('../test/users.yml', __FILE__)
+                     else
+                       File.expand_path('../users.yml', __FILE__)
+                     end
   YAML.load_file(credentials_path)
 end
 
@@ -70,120 +71,120 @@ def valid_credentials?(username, password)
   end
 end
 
-def invalid_extension?(ext) # Validate file type is supported, and has a file type.
-  valid_file_type = ["txt", "md"]
+# Validate file type is supported, and has a file type.
+def invalid_extension?(ext)
+  valid_file_type = %w[txt md]
   valid_file_type.include?(ext)
 end
 
 ##### Routes #####
 
 # Home directory/index
-get "/" do
-  pattern = File.join(data_path, "*")
+get '/' do
+  pattern = File.join(data_path, '*')
   @files = Dir.glob(pattern).map { |path| File.basename(path) }
   erb :index
 end
 
 # View New Document Page, placement of this route needs to be before "/:filename" to be matched first.
-get "/new" do
+get '/new' do
   require_signed_in_user
 
   erb :new_page
 end
 
 # Create a New Document
-post "/create" do
+post '/create' do
   require_signed_in_user
 
   filename = params[:filename].to_s
-  splitfile = filename.split(".")
+  splitfile = filename.split('.')
 
-  if filename.size == 0 # Check file name is entered.
-    session[:message] = "Please provide a valid name."
+  if filename.size.empty? # Check file name is entered.
+    session[:message] = 'Please provide a valid name.'
     status 422
-    erb :new_page    
+    erb :new_page
   elsif !invalid_extension?(splitfile[1]) # Checks file name contains a valid extension.
-    session[:message] = "Invalid file type."
+    session[:message] = 'Invalid file type.'
     status 422
-    erb :new_page    
+    erb :new_page
   else
     file_path = File.join(data_path, filename)
-    File.write(file_path, "")
+    File.write(file_path, '')
     session[:message] = "#{filename} has been created."
-    redirect "/"
+    redirect '/'
   end
 end
 
 # Sign in page
-get "/users/signin" do
+get '/users/signin' do
   erb :sign_in
 end
 
 # Submit sign in page
-post "/users/signin" do
+post '/users/signin' do
   username = params[:username]
 
   if valid_credentials?(username, params[:password])
     session[:username] = username
-    session[:message] = "Welcome!"
-    redirect "/"
+    session[:message] = 'Welcome!'
+    redirect '/'
   else
-    session[:message] = "Invalid credentials"
+    session[:message] = 'Invalid credentials'
     status 422
     erb :sign_in
   end
 end
 
 # Sign out
-post "/users/signout" do
+post '/users/signout' do
   session.delete(:username)
-  session[:message] = "You have been signed out."
-  redirect "/"
+  session[:message] = 'You have been signed out.'
+  redirect '/'
 end
 
 # Deleteing a file
-post "/:filename/delete" do
+post '/:filename/delete' do
   require_signed_in_user
 
   file_path = File.join(data_path, params[:filename])
   File.delete(file_path)
 
   session[:message] = "#{params[:filename]} has been deleted."
-  redirect "/"
+  redirect '/'
 end
 
 # View a files content
-get "/:filename" do
+get '/:filename' do
   file_path = File.join(data_path, params[:filename])
 
   if File.exist?(file_path)
-     load_file_content(file_path)
+    load_file_content(file_path)
   else
     session[:message] = "#{params[:filename]} does not exist."
-    redirect "/"
+    redirect '/'
   end
 end
 
 # Edit File Page
-get "/:filename/edit" do
+get '/:filename/edit' do
   require_signed_in_user
 
   file_path = File.join(data_path, params[:filename])
 
   @filename = params[:filename]
   @content = File.read(file_path)
-  
   erb :edit_page
 end
 
 # Submit Edited File Page
-post "/:filename" do
+post '/:filename' do
   require_signed_in_user
 
   file_path = File.join(data_path, params[:filename])
 
-  File.write(file_path, params[:content]) # Pulls content from :edit_page name field "content".
+  File.write(file_path, params[:content]) # Pulls content from :edit_page name field 'content'.
 
   session[:message] = "#{params[:filename]} has been updated."
-  redirect "/"
+  redirect '/'
 end
